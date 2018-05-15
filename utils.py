2bba8c49074e79fdf09d3fe460151be622457b7b
Dmitriy Litvak <dlitvak@tibco.com>
2018-05-14 23:53:28 -0700

    add dropout

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import csv
-
+import os
+from PIL import Image
+import numpy as np
+import matplotlib.image as mpimg
 
 def readFile(dir="data/", csv_file="driving_log.csv", fieldNames=("center","left","right","steering")):
     """Read data in the fieldNames from the csv_file (in directory dir)."""
@@ -11,3 +14,19 @@ def readFile(dir="data/", csv_file="driving_log.csv", fieldNames=("center","left
             data.append(list(row[k] for k in fieldNames))
 
     return data
+
+def resize_images_in_dir(dir="data_/", img_dir="IMG"):
+    os.chdir(dir)
+    orig_dir = img_dir + "_orig"
+    os.rename(img_dir, orig_dir)
+    os.mkdir(img_dir)
+
+    imgs = os.listdir(orig_dir)
+    for img_name in imgs:
+        if not img_name.endswith("jpg"):
+            continue
+
+        img = Image.open(os.path.join(orig_dir, img_name))
+        img.thumbnail((img.size[0] / 2, img.size[1] / 2), Image.ANTIALIAS)  # resizes image in-place
+        resized_img = np.asarray(img, dtype=np.uint8)
+        mpimg.imsave(os.path.join(img_dir, img_name), resized_img)
