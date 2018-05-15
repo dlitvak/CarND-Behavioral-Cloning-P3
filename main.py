9fe5fdefaa3382ee153eebfcc6d5d226a638f6c1
Dmitriy Litvak <dlitvak@tibco.com>
2018-05-15 01:38:35 -0700

    Add more data

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ from utils import *
 from steering_angle_predictor import SteeringAnglePredictor
 
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 EPOCHS = 10
 
-# X1, y1, img_shape = read_data(dir="data")
 X2, y2, img_shape = read_data(dir="col_data2")
 X3, y3, img_shape1 = read_data(dir="col_data4_rev")
+X1, y1, img_shape2 = read_data(dir="data")
 
-# X_train = np.vstack((X1, X2))
-# y_train = np.hstack((y1, y2))
 X = np.hstack((X2, X3))
 y = np.hstack((y2, y3))
+X_train = np.vstack((X1, X))
+y_train = np.hstack((y1, y))
 
-
-sap = SteeringAnglePredictor(img_shape=img_shape, model_file="lenet_cumul.h5", epochs=EPOCHS, batch_size=BATCH_SIZE)
+sap = SteeringAnglePredictor(img_shape=img_shape, model_file="lenet_cumul2.h5", epochs=EPOCHS, batch_size=BATCH_SIZE, prev_model="lenet_cumul.h5")
 sap.train(X, y, overwrite_model=True)
 #
 # sap.test(X[:300], y[:300])
