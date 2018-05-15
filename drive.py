f68753b163c0e2264286406fba5afcffafdeba3e
Dmitriy Litvak <dlitvak@tibco.com>
2018-05-14 23:58:31 -0700

    resize image for driving pred

--- drive.py
+++ drive.py
@@ -60,6 +60,10 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
+
+        #resize image as for training
+        image.thumbnail((image.size[0] / 2, image.size[1] / 2), Image.ANTIALIAS)
+
         image_array = np.asarray(image)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
