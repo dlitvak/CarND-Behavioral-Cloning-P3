da8aa010c8004c0e53f41c8ba5239db3e1e13174
Dmitriy Litvak <dlitvak@tibco.com>
2018-05-17 16:06:15 -0700

    try again

--- lenet.py
+++ lenet.py
@@ -1,7 +1,7 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Lambda, Dropout
 from keras.layers.pooling import MaxPool2D
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, Cropping2D
 
 
 class LeNet:
@@ -11,6 +11,8 @@ class LeNet:
     def network(self, img_shape=(160, 320, 3)):
         # normalize
         self.model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=img_shape))
+
+        self.model.add(Cropping2D(cropping=((25, 10),(0, 0))))
 
         # 160x320x3 to 158x318x6
         self.model.add(Convolution2D(6, kernel_size=3, strides=1, padding="valid"))
@@ -23,31 +25,37 @@ class LeNet:
         # to 77x157x16
         self.model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="same"))
 
+        self.model.add(Dropout(0.5))
+
         # to 76x156x26
         self.model.add(Convolution2D(26, kernel_size=2, strides=1,  padding="valid"))
         self.model.add(Activation('relu'))
+
+        self.model.add(Dropout(0.5))
 
         self.model.add(Convolution2D(52, kernel_size=3, strides=1, padding="valid"))
         self.model.add(Activation('relu'))
 
         self.model.add(MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="same"))
 
+        self.model.add(Dropout(0.5))
+
         self.model.add(Flatten())
 
         self.model.add(Dense(400))
         self.model.add(Activation('relu'))
 
-        self.model.add(Dropout(0.5))
+        self.model.add(Dropout(0.6))
 
         self.model.add(Dense(120))
         self.model.add(Activation('relu'))
 
-        self.model.add(Dropout(0.5))
+        self.model.add(Dropout(0.6))
 
         self.model.add(Dense(60))
         self.model.add(Activation('relu'))
 
         # Linear Regression Layer
-        self.model.add(Dense(1, kernel_initializer="normal", activation="linear"))
+        self.model.add(Dense(1))
 
         return self.model
--- steering_angle_predictor.py
+++ steering_angle_predictor.py
@@ -8,7 +8,6 @@ import math as m
 
 import matplotlib.image as mpimg
 
-from PIL import Image
 
 class SteeringAnglePredictor:
     def __init__(self, img_shape=(160,320,3), model_file="lenet.h5",  prev_model=None, batch_size=128, epochs=5):
@@ -23,7 +22,7 @@ class SteeringAnglePredictor:
 
     class DataSequence(Sequence):
         def __init__(self, x_set, y_set, batch_size):
-            self.x, self.y = x_set, y_set
+            self.x, self.y = shuffle(x_set, y_set)
             self.batch_size = batch_size
 
         def __len__(self):
@@ -33,35 +32,39 @@ class SteeringAnglePredictor:
             batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
             batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
 
-            img = Image.open('image.png').convert('LA')
-            return np.array([mpimg.imread(file_name) for file_name in batch_x]), np.array(batch_y)
+            return shuffle(np.array([mpimg.imread(file_name) for file_name in batch_x]), np.array(batch_y))
 
     def train(self, X, y, overwrite_model=True):
-        X, y = shuffle(X, y)
-        valid_len = m.ceil(0.2*len(X))
-        X_valid, y_valid = X[0:valid_len], y[0:valid_len]
-        X_train, y_train = X[valid_len:], y[valid_len:]
+        X_train, X_valid, y_train, y_valid = self.train_validation_split(X, y)
 
         if self.prevModel is not None:
             self.nnModel = load_model(self.prevModel)
 
-        self.nnModel.compile(optimizer='adam', loss='mean_squared_error')
+        self.nnModel.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
         history = self.nnModel.fit_generator(generator=self.DataSequence(X_train, y_train, self.batchSize) ,
                                              epochs=self.epochs, validation_data=self.DataSequence(X_valid, y_valid, self.batchSize),
-                                             shuffle=True, verbose=2)
+                                             shuffle=True, verbose=1)
 
         self.nnModel.save(filepath=self.modelFile, overwrite=overwrite_model)
         self.modelLoaded = True
         return history
 
-    def test(self, X_test, y_test):
+    def train_validation_split(self, X, y, valid_split=0.2):
+        X, y = shuffle(X, y)
+        valid_len = m.ceil(valid_split * len(X))
+        X_valid, y_valid = X[0:valid_len], y[0:valid_len]
+        X_train, y_train = X[valid_len:], y[valid_len:]
+        return X_train, X_valid, y_train, y_valid
+
+    def test(self, x, y):
         if not self.modelLoaded:
             self.nnModel = load_model(self.modelFile)
 
-        for i in range(len(X_test)):
-            pred = self.nnModel.predict(np.array([X_test[i]]), batch_size=1)
-            print("pred {}, real {}".format(pred, y_test[i]))
+        for i in range(len(x)):
+            im = mpimg.imread(x[i], format="RGB")
+            pred = self.nnModel.predict(np.array([im]), batch_size=1)
+            print("pred {}, real {}".format(pred, y[i]))
 
         # metrics = self.nnModel.evaluate(X_test, y_test)
         # for metric_i in range(len(self.nnModel.metrics_names)):
